854782d31b4ca274a7e8d02c9ffbaeb520d946bd
Wellington <wellington.wong@exults.com>
2026-09-15 03:53:44 +0800

    Setup dockerode endpoint

--- observability/src/docker/docker.controller.ts
+++ observability/src/docker/docker.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { DockerService } from './docker.service.js';
+
+
 
 @Controller('docker')
-export class DockerController {}
+export class DockerController {
+  constructor(private readonly dockerService: DockerService) {}
+
+  @Get('containers')
+  getContainers() {
+    return this.dockerService.getContainers();
+  }
+
+}
--- observability/src/docker/docker.service.ts
+++ observability/src/docker/docker.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import Docker from 'dockerode';
+
 
 @Injectable()
-export class DockerService {}
+export class DockerService {
+  private readonly docker: Docker;
+
+  constructor() {
+    this.docker = new Docker({
+      socketPath: '/var/run/docker.sock',
+    });
+  }
+
+  async getContainers() {
+    const containers = await this.docker.listContainers({
+      all: true,
+    });
+
+    return containers.map((container) => ({
+      id: container.Id,
+      name: container.Names[0]?.replace(/^\//, ''),
+
+      image: container.Image,
+      state: container.State,
+      status: container.Status,
+    }));
+  }
+}
